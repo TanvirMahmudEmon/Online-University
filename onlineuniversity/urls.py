d1515f69ad1958d7518071a806c9f34b340125f0
Tanvir Mahmud Emon <emontme@gmail.com>
2018-04-15 07:13:23 +0600

    Add files via upload

---
+++ onlineuniversity/urls.py
@@ -0,0 +1,37 @@
+"""onlineuniversity URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+# from django.conf.urls import include, url
+# from django.contrib import admin
+# from other.views import home
+
+
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+
+    # at the course_content.urls module.
+
+    url(r'', include('main.urls')),  # homepage
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^course_content/', include('course_content.urls')),  # /course_content/1/
+    url(r'^course_details/', include('course_details.urls')),  # point the root URLconf
+    url(r'^register/', include('register.urls')),  # including other app
+    url(r'^other/', include('other.urls')),  # including "other" app to include rest of the "Template"
+    # url(r'^accounts/', include('allauth.urls')),  # for django-allauth
+
+]
+
